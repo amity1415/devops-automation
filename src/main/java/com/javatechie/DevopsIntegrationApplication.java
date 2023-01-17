cea8a4d9cf6333f2f7f82a4dcf1783d8a4ebdd68
Amit Yadav <54627740+amity1415@users.noreply.github.com>
2023-01-17 06:52:06 +0000

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome to Devops integration Demo";
 	}
 
 	public static void main(String[] args) {
